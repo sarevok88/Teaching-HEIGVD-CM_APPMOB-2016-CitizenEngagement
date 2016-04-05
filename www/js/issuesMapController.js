a0ccd328ec23d33a8ea833f160972bb84a855893
Val <bouda.88@gmail.com>
2016-04-05 21:45:29 +0200

    ajout de mapbox, penser a modif les .config

--- www/js/issuesMapController.js
+++ www/js/issuesMapController.js
@@ -1,10 +1,30 @@
-angular.module("citizen-engagement.issuesMap", ['angular-storage'])
+angular.module("citizen-engagement.issuesMap", ['angular-storage', "geolocation","leaflet-directive"])
 
-.controller("issuesMapController", function($log, $scope, geolocation) {
-	// ...
+.controller("issuesMapController", function($log, $scope, geolocation,mapboxMapId, mapboxAccessToken) {
+	
+	$scope.$on('$ionicView.beforeEnter', function() {
+      $scope.mapCenter = {};
+    });
 	geolocation.getLocation().then(function(data) {
-	$scope.mapCenter.lat = data.coords.latitude;
-	$scope.mapCenter.lng = data.coords.longitude;
+		
+		
+		console.log(data.coords);
+		
+		
+		$scope.mapCenter.lat = data.coords.latitude;
+		$scope.mapCenter.lng = data.coords.longitude;
+		$scope.mapCenter.zoom = 14;
+		
+		var mapboxTileLayer = "http://api.tiles.mapbox.com/v4/" + mapboxMapId;
+		mapboxTileLayer = mapboxTileLayer + "/{z}/{x}/{y}.png?access_token=" + mapboxAccessToken;
+		
+		$scope.mapDefaults = {
+			tileLayer: mapboxTileLayer
+		};
+		
+	
+		$scope.mapMarkers = [];
+	
 	}, function(error) {
 		$log.error("Could not get location: " + error);
 	});
