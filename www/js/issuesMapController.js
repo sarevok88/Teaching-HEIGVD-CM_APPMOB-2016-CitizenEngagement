95e5f8a0f203cd33bf833af2b860ba8d00fbe448
Val <bouda.88@gmail.com>
2016-04-05 15:59:23 +0200

    correction des merges

--- www/js/issuesMapController.js
+++ www/js/issuesMapController.js
@@ -1,5 +1,11 @@
 angular.module("citizen-engagement.issuesMap", ['angular-storage'])
 
-	.controller("issuesMapController", function(){
-		console.log("issuesMapController fonctionne");
-	})
+.controller("issuesMapController", function($log, $scope, geolocation) {
+	// ...
+	geolocation.getLocation().then(function(data) {
+	$scope.mapCenter.lat = data.coords.latitude;
+	$scope.mapCenter.lng = data.coords.longitude;
+	}, function(error) {
+		$log.error("Could not get location: " + error);
+	});
+})
