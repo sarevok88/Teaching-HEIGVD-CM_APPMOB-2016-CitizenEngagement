ed63e6e5f26104881b84a1d828141cd5698903cf
jean-luc <jean-luc@Ordiii.local>
2016-04-06 13:04:55 +0200

    Modification de issues lists

--- www/js/issuesListController.js
+++ www/js/issuesListController.js
@@ -1,10 +1,21 @@
 angular.module('citizen-engagement.issuesList', [])
 
-.controller("issuesListController", function($log, $scope, $http, apiUrl, idIssue) {
-	$scope.loadIssueTypes = function () {
-    $http.get(apiUrl + '/issues/' + idIssue).success(function (issueDetails) {
-	    $scope.issueDetails = issueDetails;
-	    console.log(issueDetails);
+.controller("issuesListController", function($log, $scope, $http, apiUrl) {
+	$scope.loadIssuesList = function () {
+		$http.get(apiUrl + '/issues').success(function (issues) {
+			$scope.issues = issues;
+			console.log(issues);
+
+			index = 0;
+
+			while (index < issue.length)
+			{
+			    console.log(issue[index].name);
+			    $scope.currentIssue = issue[index].name;
+			    index++;
+			}
+
 		});
 	};
+	$scope.loadIssuesList();
 });
