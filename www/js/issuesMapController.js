cd30bf92c9d75a5b7d6c22a4259b1724b8fbe9c8
Val <bouda.88@gmail.com>
2016-04-06 09:33:22 +0200

    mapbox et pin du user

--- www/js/issuesMapController.js
+++ www/js/issuesMapController.js
@@ -1,29 +1,39 @@
 angular.module("citizen-engagement.issuesMap", ['angular-storage', "geolocation","leaflet-directive"])
 
-.controller("issuesMapController", function($log, $scope, geolocation,mapboxMapId, mapboxAccessToken) {
+.controller("issuesMapController", function($log, $scope, geolocation, mapboxMapId, mapboxAccessToken, AuthService) {
 	
-	$scope.$on('$ionicView.beforeEnter', function() {
+	
       $scope.mapCenter = {};
-    });
+	  $scope.mapDefaults = {};
+	  $scope.mapMarkers = [];
+	  $scope.userCoords = {};
+   
+	
 	geolocation.getLocation().then(function(data) {
 		
-		
 		console.log(data.coords);
-		
 		
 		$scope.mapCenter.lat = data.coords.latitude;
 		$scope.mapCenter.lng = data.coords.longitude;
 		$scope.mapCenter.zoom = 14;
 		
 		var mapboxTileLayer = "http://api.tiles.mapbox.com/v4/" + mapboxMapId;
+		
 		mapboxTileLayer = mapboxTileLayer + "/{z}/{x}/{y}.png?access_token=" + mapboxAccessToken;
 		
 		$scope.mapDefaults = {
 			tileLayer: mapboxTileLayer
 		};
+
+		//ajout du pin du user
+		$scope.userCoords.lat = $scope.mapCenter.lat;
+		$scope.userCoords.lng = $scope.mapCenter.lng;
 		
-	
-		$scope.mapMarkers = [];
+		$scope.mapMarkers.push({
+			lat: $scope.userCoords.lat,
+			lng: $scope.userCoords.lng,
+			
+		});
 	
 	}, function(error) {
 		$log.error("Could not get location: " + error);
