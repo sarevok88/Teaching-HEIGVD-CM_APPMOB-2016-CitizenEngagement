fcbd03ee4d94b6a09ab611d5398081a547c37b95
yunis.darbella <yunis.darbellay@heig-vd.ch>
2016-04-07 11:23:03 +0200

    removing console.log

--- www/js/issueDetailsController.js
+++ www/js/issueDetailsController.js
@@ -14,20 +14,11 @@ angular.module('citizen-engagement.issueDetails', [])
             $scope.loadCurrentIssue = function () {
                 $http.get(apiUrl + '/issues/' + currentId).success(function (issueCurrent) {
                     $scope.issueCurrent = issueCurrent;
-                    /*
-                     console.log(issueCurrent.issueType.name);
-                     console.log(issueCurrent.issueType.description);
-                     console.log("by " +issueCurrent.owner.name);
-                     console.log("State " +issueCurrent.state);
-                     console.log(issueCurrent.comments);
-                     */
-
 
                     index = 0;
 
                     while (index < issueCurrent.comments.length)
                     {
-
                         //console.log("Auteur: " + issueCurrent.comments[index].author.name);
                         //console.log(issueCurrent.comments[index].text);
                         //$scope.currentIssue = issueCurrent.comments[index].name;
